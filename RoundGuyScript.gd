5ae195b5d332c957a0491b5efd397b339dd399fa
Joxito <51099686+Joxito@users.noreply.github.com>
2021-09-08 20:46:39 -0300

    Mono ahora salta bien

--- RoundGuyScript.gd
+++ RoundGuyScript.gd
@@ -1,52 +1,80 @@
 extends KinematicBody2D
 
-const ACCELERATION = 50
-const MAX_SPEED = 200
-const JUMP_H = -650
+const ACCELERATION = 30
+const MAX_SPEED = 50
+const JUMP_H = -230
 const UP = Vector2(0, -1)
-const gravity = 100
+const gravity = 20
+
+export var auto_jump = false
 
 onready var sprite = $Sprite
 onready var animationPlayer = $AnimationPlayer
 
 var motion = Vector2()
+
 var controllable = true
 var jumping = false
 
+var jump_x = 0
+var jump_travel = 0
+var max_jump_travel = 10
+
+var was_on_floor = false
+
 func _physics_process(delta):
+	
+	var on_floor = is_on_floor()
+
 	motion.y += gravity
 	var friction = false
-	print(motion.y)
-	
-	if controllable:
-		if Input.is_action_pressed("ui_right"):
-			sprite.flip_h = false
-			animationPlayer.play("Walk")
-			motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
-		elif Input.is_action_pressed("ui_left"):
-			sprite.flip_h = true
-			animationPlayer.play("Walk")
-			motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
-		else:
-			animationPlayer.play("Idle")
-			friction = true
-	else:
-		motion.x = 0
-	
-	if is_on_floor():
-		controllable = true
+		
+	if on_floor:
 		jumping = false
+		controllable = true
+	
+	# Movimiento horizontal
+	var target_vel = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	# Movimiento vertical
+	if on_floor:
 		if Input.is_action_just_pressed("ui_accept"):
 			motion.y = JUMP_H
 			jumping = true
-		if friction:
-			motion.x = lerp(motion.x, 0, 0.5)
-	else:
-		if controllable:
-			if (not jumping):
-				controllable = false
-		if friction:
-			motion.x = lerp(motion.x, 0, 0.1)
 	
+	# Caso lanzarse sin saltar
+	if was_on_floor and not on_floor and not jumping:
+		controllable = false
+	was_on_floor = on_floor
+	
+	# Caso saltar al vacio
+	if motion.y > -JUMP_H and jumping:
+		controllable = false
+		
+	if not controllable:
+		$Sprite.modulate = Color.blue
+		target_vel = 0
+	else:
+		$Sprite.modulate = Color.white
+	
+	motion.x = Vector2(motion.x, 0).move_toward(Vector2(target_vel * MAX_SPEED, 0), ACCELERATION).x
 	motion = move_and_slide(motion, UP)
-				
+	
+	# Sprite
+	if motion.x > 0:
+		sprite.flip_h = false
+	if motion.x < 0:
+		sprite.flip_h = true
+	
+	if motion.x != 0:
+		animationPlayer.play("Walk")
+	else:
+		animationPlayer.play("Idle")
+		
+	# Debugging	
+	if Input.is_action_just_pressed("teleport"):
+		global_position = get_global_mouse_position()
+		
+	print(jumping)
+			
+
--- scenes/PlayerScript.gd
+++ scenes/PlayerScript.gd
@@ -2,16 +2,21 @@ extends KinematicBody2D
 
 const ACCELERATION = 30
 const MAX_SPEED = 50
-const JUMP_H = -200
+const JUMP_H = -220
 const UP = Vector2(0, -1)
 const gravity = 20
 
-export var auto_jump = true
+# Variables para debugging
+export var auto_jump = false
+export var see_controllable = false
 
 onready var sprite = $Sprite
 onready var animationPlayer = $AnimationPlayer
 
 var motion = Vector2()
+
+var controllable = true
+var jumping = false
 
 var jump_x = 0
 var jump_travel = 0
@@ -21,61 +26,66 @@ var was_on_floor = false
 
 func _physics_process(delta):
 	
-	motion = move_and_slide(motion, UP)
 	var on_floor = is_on_floor()
-	
-	# apply gravity to the player
+
 	motion.y += gravity
 	var friction = false
+		
+	if on_floor:
+		jumping = false
+		controllable = true
 	
+	# Movimiento horizontal
 	var target_vel = Input.get_action_strength("right") - Input.get_action_strength("left")
 	
+	# Movimiento vertical
+	if on_floor:
+		if Input.is_action_just_pressed("ui_accept"):
+			motion.y = JUMP_H
+			jumping = true
 	
-#	if not on_floor:
-#		target_vel = lerp(target_vel, 0, jump_travel / max_jump_travel)
-	if abs(motion.y) > 200:
+	if was_on_floor and not on_floor and auto_jump:
+		motion.y = JUMP_H
+		jumping = true
+	
+	# Caso lanzarse sin saltar
+	if was_on_floor and not on_floor and not jumping:
+		controllable = false
+	was_on_floor = on_floor
+	
+	# Caso saltar al vacio
+	if motion.y > -JUMP_H-30 and jumping:
+		controllable = false
+		
+	if not controllable:
+		if see_controllable:
+			$Sprite.modulate = Color.blue
 		target_vel = 0
-#		$Sprite.modulate = Color.blue
 	else:
-		$Sprite.modulate = Color.white
+		if see_controllable:
+			$Sprite.modulate = Color.white
 	
 	motion.x = Vector2(motion.x, 0).move_toward(Vector2(target_vel * MAX_SPEED, 0), ACCELERATION).x
+	motion = move_and_slide(motion, UP)
 	
+	# Sprite
 	if motion.x > 0:
 		sprite.flip_h = false
 	if motion.x < 0:
 		sprite.flip_h = true
 	
-
 	if motion.x != 0:
 		animationPlayer.play("Walk")
 	else:
 		animationPlayer.play("Idle")
-		friction = true
-	
-	if auto_jump and was_on_floor and not on_floor:
-		motion.y = JUMP_H
-		jump_x = global_position.x
-	was_on_floor = on_floor
 		
-	if on_floor:
-		if Input.is_action_just_pressed("ui_accept"):
-			motion.y = JUMP_H
-			jump_x = global_position.x
-		if friction == true:
-			motion.x = lerp(motion.x, 0, 0.5)
-	else:
-		jump_travel = clamp((global_position.x - jump_x), -max_jump_travel, max_jump_travel)
-		if friction == true:
-			motion.x = lerp(motion.x, 0, 0.01)
-			
-	print(motion.length())
-	
+	# Debugging	
 	if Input.is_action_just_pressed("teleport"):
 		global_position = get_global_mouse_position()
+		
+	print(jumping)
 			
 
-	
 	
 	
 	
