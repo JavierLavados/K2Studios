c236ebc1f06884eb3d428d5aa8bdb4a6e66a7f21
Joxito <51099686+Joxito@users.noreply.github.com>
2021-09-08 22:29:37 -0300

    Limpieza de archivos y reescalamiento

--- scenes/PlayerScript.gd
+++ scenes/PlayerScript.gd
@@ -1,19 +1,21 @@
 extends KinematicBody2D
 
-const ACCELERATION = 30
-const MAX_SPEED = 50
-const JUMP_H = 220
-const UP = Vector2(0, 1)
-const gravity = 20
+const ACCELERATION = 140
+const MAX_SPEED = 200
+const JUMP_H = 1100
+const UP = Vector2(0, -1)
+const gravity = 120
 const forward = "right" #down
 const backward = "left" #up 
 
 # Variables para debugging
 export var auto_jump = false
-export var see_controllable = true
+export var see_controllable = false
+export var teleport = false
 
 onready var sprite = $Sprite
 onready var animationPlayer = $AnimationPlayer
+onready var scl = sprite.scale.x
 
 var motion = Vector2()
 
@@ -26,7 +28,7 @@ func _physics_process(delta):
 	
 	var on_floor = is_on_floor()
 
-	motion += -gravity * UP
+	motion += -(gravity/scl) * UP
 		
 	if on_floor:
 		jumping = false
@@ -38,11 +40,11 @@ func _physics_process(delta):
 	# Movimiento vertical
 	if on_floor:
 		if Input.is_action_just_pressed("ui_accept"):
-			motion = UP * JUMP_H
+			motion = UP * JUMP_H/scl
 			jumping = true
 	
 	if was_on_floor and not on_floor and auto_jump:
-		motion = UP * JUMP_H
+		motion = UP * JUMP_H/scl
 		jumping = true
 	
 	# Caso lanzarse sin saltar
@@ -51,7 +53,7 @@ func _physics_process(delta):
 	was_on_floor = on_floor
 	
 	# Caso saltar al vacio
-	if motion.dot(UP) < -JUMP_H+30 and jumping:
+	if motion.dot(UP) < -(JUMP_H/scl)+30 and jumping:
 		controllable = false
 		
 	if not controllable:
@@ -63,9 +65,9 @@ func _physics_process(delta):
 			$Sprite.modulate = Color.white
 	
 	if UP.x == 0:
-		motion.x = Vector2(motion.x, 0).move_toward(Vector2(target_vel * MAX_SPEED, 0), ACCELERATION).x
+		motion.x = Vector2(motion.x, 0).move_toward(Vector2(target_vel * MAX_SPEED/scl, 0), ACCELERATION/scl).x
 	else:
-		motion.y = Vector2(0, motion.y).move_toward(Vector2(0, target_vel * MAX_SPEED), ACCELERATION).y
+		motion.y = Vector2(0, motion.y).move_toward(Vector2(0, target_vel * MAX_SPEED/scl), ACCELERATION/scl).y
 	motion = move_and_slide(motion, UP)
 	
 	# Sprite
@@ -85,7 +87,7 @@ func _physics_process(delta):
 		animationPlayer.play("Idle")
 		
 	# Debugging	
-	if Input.is_action_just_pressed("teleport"):
+	if Input.is_action_just_pressed("teleport") and teleport:
 		global_position = get_global_mouse_position()
 	
 	
