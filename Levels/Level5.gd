0263b3035bafd738b85d6b26c21c2ca0f974baa2
Joxito <51099686+Joxito@users.noreply.github.com>
2021-09-29 03:33:00 -0300

    Se agrega un nuevo nivel

--- Levels/Level5.gd
+++ Levels/Level5.gd
@@ -1,7 +1,7 @@
 extends "res://Levels/Scripts/LevelAbstract.gd"
 
 var n_players = 4
-var next = "res://Levels/Level5.tscn"
+var next = "res://Levels/Level3.tscn"
 
 func _ready():
 	setUp(n_players)
