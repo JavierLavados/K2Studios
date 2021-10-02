adb202bd10e893d74242142a18138a54e93b05c1
Joxito <51099686+Joxito@users.noreply.github.com>
2021-10-02 03:45:46 -0300

    Sprites de powerup para todos, salto perfeccionado

--- Players/Scripts/PlayerAbstract.gd
+++ Players/Scripts/PlayerAbstract.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
 # Constantes
-const MAX_SPEED = 90
-const AIR_SPEED = 110
-const AIR_SPEED_2 = 140
+const MAX_SPEED = 96
+const AIR_SPEED = 112
+const AIR_SPEED_2 = 144
 const JUMP_H = 375
 const JUMP_H_2 = 325
 const gravity = 25
@@ -22,6 +22,7 @@ onready var collision = $CollisionShape2D
 onready var ladder_detector = $ladder_detector
 onready var left_pos = $Left
 onready var right_pos = $Right
+onready var ray = $RayCast2D
 
 # Variables auxiliares
 var InvisiWall = preload("res://WIP/InvisiWall.tscn")
@@ -31,9 +32,9 @@ var jumping = false
 var was_on_floor = false
 var on_ladder = false
 var air_jump_used = false
-var air_time = 0
 var doubleJump = false
 var jump_pos = 0
+var rot
 
 var invisiWallL
 var invisiWallR
@@ -44,13 +45,13 @@ func initialize(n):
 	var players = get_tree().get_nodes_in_group("Players")
 	for player in players:
 		add_collision_exception_with(player)
-	var rot = -rad2deg(n.angle_to(Vector2(0,-1)))
+	rot = -rad2deg(n.angle_to(Vector2(0,-1)))
 	sprite.rotation_degrees = rot
 	collision.rotation_degrees = rot
 	
 func modify_sprite(n, forward, backward):
 	var left = n.rotated(deg2rad(90))
-	var side_motion = left.dot(motion)
+	var side_motion = int(left.dot(motion))
 	var flip = false
 	if n.x < 0 or n.y > 0:
 		flip = true
@@ -82,7 +83,6 @@ func modify_sprite(n, forward, backward):
 		else:
 			playback.travel("Idle")
 	
-	
 # GENERALIZAR PARA TODOS
 func is_on_ladder():
 	var areas = ladder_detector.get_overlapping_areas()
@@ -100,64 +100,88 @@ func is_on_ladder():
 #		if block:
 #			block.push(hspd)
 			
+			
+			
+# FUNCION PRINCIPAL
 func calc_motion(n, forward, backward, top, btm):
 	
-	var relative_pos = int(global_position.x-15)%32
-
-	left_pos.global_position.x = int(global_position.x - 63 - relative_pos)
-	right_pos.global_position.x = int(global_position.x + 96 - relative_pos)
-	
+	# Calculo para colision con puas 
+	# (generalizar para otros elementos peligrosos)
 	for i in get_slide_count():
 		var coll = get_slide_collision(i)
 		if is_instance_valid(coll.collider):
 			if coll.collider.is_in_group("Spikes"): 
 				if coll.collider.NORMAL == coll.normal:
 					get_tree().reload_current_scene()
-
+					
+	# Variables relativas a posicion actual
 	var on_floor = is_on_floor()
+	var on_edge = !ray.is_colliding() and on_floor
+	var relative_pos = Vector2(int(global_position.x)%32-15,int(global_position.y)%32-15)
 	
-	if jumping:
-		if (air_time < AIR_SPEED):
-			air_time += 1
-	else:
-		air_time = 0
-		
-	if motion.dot(n) > -JUMP_H:
-		motion += -gravity * n
-			
+	# Reseteo de variables al estar en el piso
 	if on_floor:
 		jumping = false
 		controllable = true
+		air_jump_used = false
+		if is_instance_valid(invisiWallL):
+			invisiWallL.queue_free()
+			invisiWallR.queue_free()
 		
-	# Movimiento horizontal
+	# Aceleracion de gravedad hasta cierto punto
+	if motion.dot(n) > -JUMP_H:
+		motion += -gravity * n
+		
+	# Inputs del teclado
 	var target_vel = Input.get_action_strength(forward) - Input.get_action_strength(backward)
-	
 	var vertical_vel = Input.get_action_strength(top) - Input.get_action_strength(btm)
 
 	# Movimiento vertical (salto)
-		
 	if Input.is_action_just_pressed("ui_accept") and awake:
 		
-		print(relative_pos)
+		# Posicionamiento de paredes invisibles
+		var l_val = 95
+		var r_val = 97
 		
-		air_time = 0
+		if (on_edge and not jumping):
+			if n.x == 0:
+				if relative_pos.x > 0:
+					l_val = 63
+				else:
+					r_val = 65
+			else:
+				if relative_pos.y > 0:
+					l_val = 63
+				else:
+					r_val = 65
+				
+		
+		if n.x == 0:
+			left_pos.global_position.x = int(global_position.x - l_val - relative_pos.x)
+			right_pos.global_position.x = int(global_position.x + r_val - relative_pos.x)
+		else:
+			left_pos.global_position.y = int(global_position.y - l_val - relative_pos.y)
+			right_pos.global_position.y = int(global_position.y + r_val - relative_pos.y)
 		
 		# Caso segundo salto
-		if not air_jump_used && doubleJump and not on_floor:
+		if doubleJump and not air_jump_used and not on_floor:
 
-			air_jump_used = true
 			motion = n * JUMP_H_2
 			jumping = true
+			air_jump_used = true
 			controllable = true
 			
+			# Creacion paredes invisibles
 			if not is_instance_valid(invisiWallL):
 				invisiWallL = InvisiWall.instance()
 				invisiWallR = InvisiWall.instance()
 				get_parent().add_child(invisiWallL) 
-				get_parent().add_child(invisiWallR) 
-
+				get_parent().add_child(invisiWallR)
+			invisiWallL.rotation_degrees = rot
+			invisiWallR.rotation_degrees = rot 
 			invisiWallL.global_position = left_pos.global_position
-			invisiWallR.global_position = right_pos.global_position
+			invisiWallR.global_position = right_pos.global_position	
+			
 
 		# Caso salto inicial
 		if on_floor:
@@ -169,16 +193,11 @@ func calc_motion(n, forward, backward, top, btm):
 			invisiWallR = InvisiWall.instance()
 			get_parent().add_child(invisiWallL) 
 			get_parent().add_child(invisiWallR) 
+			invisiWallL.rotation_degrees = rot
+			invisiWallR.rotation_degrees = rot 
 			invisiWallL.global_position = left_pos.global_position
 			invisiWallR.global_position = right_pos.global_position
-		
-	# Se eliminan paredes invisibles al tocar el piso
-	if on_floor:
-		if is_instance_valid(invisiWallL) and not jumping:
-			invisiWallL.queue_free()
-			invisiWallR.queue_free()
-		air_jump_used = false
-	
+			
 	# AUTO JUMP PARA DEBUGGING
 	if was_on_floor and not on_floor and auto_jump:
 		motion = n * JUMP_H
@@ -192,7 +211,7 @@ func calc_motion(n, forward, backward, top, btm):
 		controllable = false
 	was_on_floor = on_floor
 	
-	# Caso saltar al vacio
+	# Caso salto con caida libre
 	if motion.dot(n) <= -JUMP_H:
 		controllable = false
 		
@@ -215,9 +234,9 @@ func calc_motion(n, forward, backward, top, btm):
 			motion.x = target_vel * MAX_SPEED
 		else:
 			if air_jump_used:
-				motion.x = target_vel * (AIR_SPEED_2 - air_time)
+				motion.x = target_vel * AIR_SPEED_2
 			else:
-				motion.x = target_vel * (AIR_SPEED - air_time)
+				motion.x = target_vel * AIR_SPEED
 		if not awake:
 			motion.x = 0
 	else:
@@ -225,9 +244,9 @@ func calc_motion(n, forward, backward, top, btm):
 			motion.y = target_vel * MAX_SPEED
 		else:
 			if air_jump_used:
-				motion.y = target_vel * (AIR_SPEED_2 - air_time)
+				motion.y = target_vel * AIR_SPEED_2
 			else:
-				motion.y = target_vel * (AIR_SPEED - air_time)
+				motion.y = target_vel * AIR_SPEED
 		if not awake:
 			motion.y = 0
 				
--- Players/Scripts/PlayerUp.gd
+++ Players/Scripts/PlayerUp.gd
@@ -8,6 +8,5 @@ func _ready():
 	initialize(NORMAL)
 	
 func _physics_process(delta):
-	
 	calc_motion(NORMAL, "right", "left", "up", "down")
 	modify_sprite(NORMAL, "right", "left")
