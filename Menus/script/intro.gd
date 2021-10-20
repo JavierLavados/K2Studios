d94050e5d1efe6f4642e63955f6e37ff915de6c2
CherryStopher <cristobal.saldias.s@ug.uchile.cl>
2021-10-20 03:16:58 -0300

    cambio de logo

--- Menus/script/intro.gd
+++ Menus/script/intro.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready():
 	$AnimationPlayer.play("Fade in")
-	yield(get_tree().create_timer(6), "timeout")
+	yield(get_tree().create_timer(4), "timeout")
 	$AnimationPlayer.play("Fade out")
-	yield(get_tree().create_timer(3), "timeout")
+	yield(get_tree().create_timer(2), "timeout")
 	get_tree().change_scene("res://Menus/Initial.tscn")
