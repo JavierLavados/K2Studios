3fbae5e53f29ee373bc1a620b2f6ffa26ce7b58f
CherryStopher <cristobal.saldias.s@ug.uchile.cl>
2021-10-23 16:04:02 -0300

    pantalla tutorial funciona

--- Levels/Scripts/Tutorial1.gd
+++ Levels/Scripts/Tutorial1.gd
@@ -3,6 +3,12 @@ extends Sprite
 
 func _ready():
 	get_parent().get_node("Tutorial1").visible = true
+	get_tree().paused = true
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		get_parent().get_node("Tutorial1").visible = false
+		get_tree().paused = false
 
 
 
