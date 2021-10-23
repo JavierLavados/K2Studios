afed9de603dd8dccb19c3e6e3c6df495780a5deb
Joxito <51099686+Joxito@users.noreply.github.com>
2021-10-23 03:22:01 -0300

    Se arregla bug de salto

--- InvisiWalls/Scripts/InvisiWallAbs.gd
+++ InvisiWalls/Scripts/InvisiWallAbs.gd
@@ -3,13 +3,10 @@ extends StaticBody2D
 var exists = true
 
 func setUp(id):
-	var playerUp = get_tree().root.get_child(1).get_node("PlayerUp")
-	var playerRight = get_tree().root.get_child(1).get_node("PlayerRight")
-	var playerDown = get_tree().root.get_child(1).get_node("PlayerDown")
-	var playerLeft = get_tree().root.get_child(1).get_node("PlayerLeft")
-	var players = [playerUp, playerRight, playerDown, playerLeft]
-	for i in range(4):
-		if i != id:
+	var players = get_tree().root.get_child(1).players
+	var ids = get_tree().root.get_child(1).ids
+	for i in range(len(players)):
+		if ids[i] != id:
 			add_collision_exception_with(players[i])
 	var blocks = get_tree().get_nodes_in_group("Blocks")
 	for block in blocks:
