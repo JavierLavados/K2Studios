6e46cba65adc255c855bb4ca602522e3a221f284
Joxito <51099686+Joxito@users.noreply.github.com>
2021-11-20 03:13:50 -0300

    Mapas de los mundos, nuevos sprites

--- Levels/World 2/Level2-1.gd
+++ Levels/World 2/Level2-1.gd
@@ -1,14 +1,10 @@
 extends "res://Levels/Scripts/LevelAbstract.gd"
 
-var n_players = 2
-var next = "res://MapInterface/Maps/MapWorld1.tscn"
+var n_players = 4
+var next = "res://MapInterface/Maps/MapWorld2.tscn"
 
 func _ready():
-	setUp(n_players)
+	setUp(n_players,1)
 	
 func _physics_process(delta):
 	level(n_players, next)
-
-
-
-
