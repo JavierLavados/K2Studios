e8eda21a76abf8f197d2726642167cca6512bb7f
javierLavados <javierlavadosjilbert@gmail.com>
2021-11-21 21:48:40 -0300

    mapa listo pero incompleto

--- MapInterface/WorldMaps/Scripts/WorldMaps.gd
+++ MapInterface/WorldMaps/Scripts/WorldMaps.gd
@@ -35,10 +35,42 @@ func _on_Transition23_body_entered(body):
 	var pos = $MapPointer.get_position()
 	#print(pos)
 	if pos.x<2048:
+		Globals.current_level = 21
 		Globals.current_world=Globals.current_world+1
 		yield(get_tree().create_timer(0.1), "timeout")
 		$Camera2D.set_position(Vector2(512+2048,320))
 	else:
+		Globals.current_level = 20
 		Globals.current_world=Globals.current_world-1
 		yield(get_tree().create_timer(0.1), "timeout")
 		$Camera2D.set_position(Vector2(512+1024,320))
+
+
+func _on_Transition34_body_entered(body):
+	var pos = $MapPointer.get_position()
+	#print(pos)
+	if pos.x<3072:
+		Globals.current_level = 31
+		Globals.current_world=Globals.current_world+1
+		yield(get_tree().create_timer(0.1), "timeout")
+		$Camera2D.set_position(Vector2(512+3072,320))
+	else:
+		Globals.current_level = 30
+		Globals.current_world=Globals.current_world-1
+		yield(get_tree().create_timer(0.1), "timeout")
+		$Camera2D.set_position(Vector2(512+2048,320))
+
+
+func _on_Transition45_body_entered(body):
+	var pos = $MapPointer.get_position()
+	#print(pos)
+	if pos.x<4096:
+		Globals.current_level = 41
+		Globals.current_world=Globals.current_world+1
+		yield(get_tree().create_timer(0.1), "timeout")
+		$Camera2D.set_position(Vector2(512+4096,320))
+	else:
+		Globals.current_level = 40
+		Globals.current_world=Globals.current_world-1
+		yield(get_tree().create_timer(0.1), "timeout")
+		$Camera2D.set_position(Vector2(512+3072,320))
