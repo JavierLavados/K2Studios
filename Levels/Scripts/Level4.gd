6138e6d859c39201423cf78aa3de323fa9a776a3
Cristobal Saldias <cristobals80@hotmail.com>
2021-11-21 23:06:23 -0300

    mundo 3 y el parametro sacado

--- Levels/Scripts/Level4.gd
+++ Levels/Scripts/Level4.gd
@@ -1,10 +1,9 @@
 extends "res://Levels/Scripts/LevelAbstract.gd"
 
 var n_players = 4
-var next = "res://MapInterface/Map.tscn"
 
 func _ready():
 	setUp(n_players)
 	
 func _physics_process(delta):
-	level(n_players, next)
+	level(n_players)
--- Levels/World1/Scripts/Level1-2.gd
+++ Levels/World1/Scripts/Level1-2.gd
@@ -1,13 +1,12 @@
 extends "res://Levels/Scripts/LevelAbstract.gd"
 
 var n_players = 2
-var next = "res://MapInterface/Maps/MapWorld1.tscn"
 
 func _ready():
 	setUp(n_players)
 	
 func _physics_process(delta):
-	level(n_players, next)
+	level(n_players)
 
 
 
