f76ca3be8a93103d063f59d9d94b50b69ae49624
Joxito <51099686+Joxito@users.noreply.github.com>
2021-11-22 14:04:20 -0300

    World map arreglado

--- MapInterface/WorldMaps/Scripts/WorldMaps.gd
+++ MapInterface/WorldMaps/Scripts/WorldMaps.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+onready var pointer = $MapPointer
+onready var background = $Background
 
 var level_total = 50
-onready var pointer = $MapPointer
 
 func _ready():
-	$Label.text=str(Globals.current_points)
+	$Label.text = str(Globals.current_points)
 	$Camera2D.set_position(Vector2(512+1024*(Globals.current_world-1),320))
+	background.position.x = 1024*(Globals.current_world-1)
 	
 	var icons = get_tree().get_nodes_in_group("LevelIcons")
 	var current_level = Globals.current_level
@@ -15,63 +17,28 @@ func _ready():
 		if icons[i].level_number == current_level:
 			pointer.current_node = icons[i]
 			break
-
-func _on_Transition12_body_entered(body):
-	
+			
+func transition():
 	var pos = $MapPointer.get_position()
-	#print(pos)
-	if pos.x<1024:
-		Globals.current_level = 11
-		Globals.current_world=Globals.current_world+1
-		yield(get_tree().create_timer(0.1), "timeout")
-		$Camera2D.set_position(Vector2(512+1024,320))
+	if int(pos.x)%1024>512:
+		Globals.current_level = Globals.current_world*10+1
+		Globals.current_world = Globals.current_world+1
 	else:
-		Globals.current_level = 10
-		Globals.current_world=Globals.current_world-1
-		yield(get_tree().create_timer(0.1), "timeout")
-		$Camera2D.set_position(Vector2(512,320))
+		Globals.current_level = (Globals.current_world-1)*10
+		Globals.current_world = Globals.current_world-1
+	yield(get_tree().create_timer(0.1), "timeout")
+	background.position.x = 1024*(Globals.current_world-1)
+	background.world = Globals.current_world
+	$Camera2D.set_position(Vector2(512+(Globals.current_world-1)*1024,320))
 
+func _on_Transition1_body_entered(body):
+	transition()
 
-func _on_Transition23_body_entered(body):
-	var pos = $MapPointer.get_position()
-	#print(pos)
-	if pos.x<2048:
-		Globals.current_level = 21
-		Globals.current_world=Globals.current_world+1
-		yield(get_tree().create_timer(0.1), "timeout")
-		$Camera2D.set_position(Vector2(512+2048,320))
-	else:
-		Globals.current_level = 20
-		Globals.current_world=Globals.current_world-1
-		yield(get_tree().create_timer(0.1), "timeout")
-		$Camera2D.set_position(Vector2(512+1024,320))
+func _on_Transition2_body_entered(body):
+	transition()
 
+func _on_Transition3_body_entered(body):
+	transition()
 
-func _on_Transition34_body_entered(body):
-	var pos = $MapPointer.get_position()
-	#print(pos)
-	if pos.x<3072:
-		Globals.current_level = 31
-		Globals.current_world=Globals.current_world+1
-		yield(get_tree().create_timer(0.1), "timeout")
-		$Camera2D.set_position(Vector2(512+3072,320))
-	else:
-		Globals.current_level = 30
-		Globals.current_world=Globals.current_world-1
-		yield(get_tree().create_timer(0.1), "timeout")
-		$Camera2D.set_position(Vector2(512+2048,320))
-
-
-func _on_Transition45_body_entered(body):
-	var pos = $MapPointer.get_position()
-	#print(pos)
-	if pos.x<4096:
-		Globals.current_level = 41
-		Globals.current_world=Globals.current_world+1
-		yield(get_tree().create_timer(0.1), "timeout")
-		$Camera2D.set_position(Vector2(512+4096,320))
-	else:
-		Globals.current_level = 40
-		Globals.current_world=Globals.current_world-1
-		yield(get_tree().create_timer(0.1), "timeout")
-		$Camera2D.set_position(Vector2(512+3072,320))
+func _on_Transition4_body_entered(body):
+	transition()
---
+++ WIP/BackgroundWorldMap.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+onready var layer1 = $Layer1
+onready var layer2 = [$Layer2a, $Layer2b]
+onready var layer3 = [$Layer3a, $Layer3b]
+onready var layer4 = [$Layer4a, $Layer4b]
+onready var layers = layer2 + layer3 + layer4
+
+var view_w = 1024
+var world
+
+var counter = 0
+
+func _ready():
+	world = Globals.current_world
+	var t = "res://Sprites/Backgrounds/BgW" + str(world) + ".png"
+	layer1.texture = load(t)
+
+func _process(delta):
+	var t = "res://Sprites/Backgrounds/BgW" + str(world) + ".png"
+	layer1.texture = load(t)
+	var mov = delta*8
+	for l in layer2:
+		l.position.x += mov
+	for l in layer3:
+		l.position.x += mov*2
+	for l in layer4:
+		l.position.x += mov*4
+	for l in layers:
+		if l.position.x >= view_w:
+			l.position.x = -view_w
