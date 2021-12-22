f62eeaba25f32febef46bc95cd4b1dbe5b59089d
javierLavados <javierlavadosjilbert@gmail.com>
2021-12-22 00:44:39 -0300

    se embellece el contador

--- MapInterface/WorldMaps/Scripts/WorldMaps.gd
+++ MapInterface/WorldMaps/Scripts/WorldMaps.gd
@@ -7,14 +7,15 @@ onready var background = $Background
 var level_total = 50
 
 func _ready():
-	var points = int(Globals.current_points)
-	$Decena.frame=points/10
-	$Unidad.frame=points%10
+
+	$Counter/Decena.frame=(int(Globals.current_points)/10)+10*(int(Globals.current_world)-1)
+	$Counter/Unidad.frame=int(Globals.current_points)%10+10*(int(Globals.current_world)-1)
+	$Counter.frame=int(Globals.current_world)-1
 	
 	$Camera2D.set_position(Vector2(512+1024*(Globals.current_world-1),320))
 	background.position.x = 1024*(Globals.current_world-1)
-	$Decena.position.x = 1024*(Globals.current_world-1)+800
-	$Unidad.position.x = 1024*(Globals.current_world-1)+864
+	$Counter.position.x = 1024*(Globals.current_world-1)+960
+
 	
 	var icons = get_tree().get_nodes_in_group("LevelIcons")
 	var current_level = Globals.current_level
@@ -38,8 +39,10 @@ func transition():
 		Globals.current_world = Globals.current_world-1
 	yield(get_tree().create_timer(0.1), "timeout")
 	background.position.x = 1024*(Globals.current_world-1)
-	$Decena.position.x=1024*(Globals.current_world-1)+800
-	$Unidad.position.x = 1024*(Globals.current_world-1)+864
+	$Counter/Decena.frame=(int(Globals.current_points)/10)+10*(int(Globals.current_world)-1)
+	$Counter/Unidad.frame=int(Globals.current_points)%10+10*(int(Globals.current_world)-1)
+	$Counter.frame=int(Globals.current_world)-1
+	$Counter.position.x = 1024*(Globals.current_world-1)+960
 	background.world = Globals.current_world
 	$Camera2D.set_position(Vector2(512+(Globals.current_world-1)*1024,320))
 
