43ab8a4ddb2d38d100a2015429dede441e3f46b2
javierLavados <javierlavadosjilbert@gmail.com>
2021-12-23 18:02:02 -0300

    se agrega boton pa regresar

--- Menus/script/Credits.gd
+++ Menus/script/Credits.gd
@@ -4,11 +4,6 @@ extends Node2D
 func _ready():
 	pass 
 
-
-func _on_Back_pressed():
-	visible = false
-	if get_parent().name == "Interface":
-		get_parent().get_node("Pausa").visible = true
-	else:
-		var actual_dir = "res://Menus/Initial.tscn"
-		get_tree().change_scene(actual_dir)
+func _on_TextureButton_pressed():
+	var actual_dir = "res://Menus/Initial.tscn"
+	get_tree().change_scene(actual_dir)
