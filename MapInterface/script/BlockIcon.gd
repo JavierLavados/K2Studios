e189f3daf797414c9f40bd03cccadf0a45bb7dd1
javierLavados <javierlavadosjilbert@gmail.com>
2021-12-24 00:40:47 -0300

    aaa

--- MapInterface/script/BlockIcon.gd
+++ MapInterface/script/BlockIcon.gd
@@ -3,6 +3,16 @@ extends Node2D
 export var required = 1
 #var bloqued
 func _ready():
+	var decima = required/10+(Globals.current_world-1)*10
+	var unidad = required%10+(Globals.current_world-1)*10
+	if decima == 0:
+		$Sprite2/Decima.visible=false
+		$Sprite2/Unidad.frame=unidad
+		$Sprite2/Unidad.position.x=0
+	if decima !=0:
+		$Sprite2/Decima.frame=decima
+		$Sprite2/Unidad.frame=unidad
+
 	if required<= Globals.current_points:
 		#bloqued = false
 		visible = false
