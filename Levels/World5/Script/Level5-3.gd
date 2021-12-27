e21d16bc7016bd837fd41157702f79de10dd1b36
Joxito <51099686+Joxito@users.noreply.github.com>
2021-12-27 16:21:09 -0300

    Se rellenan niveles

--- Levels/World5/Script/Level5-3.gd
+++ Levels/World5/Script/Level5-3.gd
@@ -1,6 +1,6 @@
 extends "res://Levels/Scripts/LevelAbstract.gd"
 
-var n_players = 1
+var n_players = 3
 
 func _ready():
 	setUp(n_players)
--- Menus/script/LevelManager.gd
+++ Menus/script/LevelManager.gd
@@ -20,30 +20,4 @@ func change_scene_no_sfx(index):
 	if index >= 0 and index < paths.size():
 		scene.change_scene(paths[index].instance())
 
-var codigo = "JJJ"
-var collected_keys = ""
-
-
-func _process(delta):
-	if Input.is_action_just_pressed("J"):
-		collected_keys+="J"
-		_wakeup_Timer()
-		
-func _wakeup_Timer():
-	$Timer.wait_time = 0.3
-	$Timer.start()
-
-func _on_Timer_timeout():
-	$Timer.stop()
-	if collected_keys==codigo:
-		#print(scene.get_child(0).get_child(0).name)
-		if "Initial" in scene.get_child(0).get_child(0).name:
-			Globals.current_points=50
-			for i in range(0,len(Globals.levels_status)):
-				Globals.levels_status[i]=true
-			for i in range(0,len(Globals.path_status)):
-				Globals.path_status[i]=true
-			for i in range(0,len(Globals.block_status)):
-				Globals.block_status[i]=true
-		
 
